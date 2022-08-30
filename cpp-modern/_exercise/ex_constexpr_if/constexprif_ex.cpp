a1848b83dc0a635327e9e2bec597045830fe629d
Krystian Piękoś <krystian.piekos@infotraining.pl>
2022-08-30 14:35:22 +0200

    std::byte

--- cpp-modern/_exercise/ex_constexpr_if/constexprif_ex.cpp
+++ cpp-modern/_exercise/ex_constexpr_if/constexprif_ex.cpp
@@ -14,17 +14,33 @@ struct SlowPath
 {
 };
 
+template <typename TIterator>
+auto advance_it(TIterator& it, int count)
+{
+    using ItCategory = typename std::iterator_traits<TIterator>::iterator_category;
 
+    if constexpr (std::is_base_of_v<std::random_access_iterator_tag, ItCategory>)
+    {
+        it += count;
+        return FastPath();
+    }
+    else
+    {
+        for (int i = 0; i < count; ++i)
+            ++it;
+        return SlowPath();
+    }
+}
 
 TEST_CASE("constexpr-if with iterator categories")
 {
     SECTION("random_access_iterator")
     {
-        vector<int> data = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        const vector<int> data = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
         auto it = data.begin();
 
-        auto result = advance_it(it, 3);
+        auto result = advance_it(it, 3); // it += 3
 
         static_assert(std::is_same_v<decltype(result), FastPath>);
 
@@ -37,7 +53,7 @@ TEST_CASE("constexpr-if with iterator categories")
 
         auto it = data.begin();
 
-        auto result = advance_it(it, 3);
+        auto result = advance_it(it, 3); // n times ++it
 
         static_assert(std::is_same_v<decltype(result), SlowPath>);
 
--- cpp-modern/small-features/small_features.cpp
+++ cpp-modern/small-features/small_features.cpp
@@ -7,6 +7,8 @@
 #include <tuple>
 #include <vector>
 #include <span>
+#include <utility>
+#include <bitset>
 
 #include "catch.hpp"
 
@@ -14,4 +16,80 @@ enum class DayOfWeek : uint8_t { mon = 1, tue, wed, thd, fri, sat, sun };
 
 TEST_CASE("changes for enums")
 {
+    DayOfWeek today = DayOfWeek::tue;
+
+    auto value = static_cast<std::underlying_type_t<DayOfWeek>>(today);
+
+    REQUIRE(value == 2);
+
+    SECTION("Since C++17")
+    {
+        DayOfWeek tomorrow{3};
+
+        REQUIRE(tomorrow == DayOfWeek::wed);
+    }
+}
+
+TEST_CASE("old school byte")
+{
+    char my_byte_1 = 32;
+    char my_byte_2 = 33;
+    auto result = my_byte_1 + my_byte_2;
+
+    std::cout << my_byte_1 << "\n";
+}
+
+namespace Explain
+{
+    enum class byte : unsigned char {};
+
+    constexpr byte operator &(byte a, byte b)
+    {
+        return a & b;
+    }
+}
+
+TEST_CASE("std::byte")
+{
+    std::byte my_byte_1{32};
+    std::byte my_byte_2{33};
+
+    auto result = my_byte_1 & my_byte_2;    
+
+    std::cout << std::to_integer<int>(result) << "\n";
+}
+
+void print(float const x, std::span<const std::byte> const bytes)
+{
+    std::cout << std::setprecision(6) << std::setw(8) << x << " = { "
+              << std::hex << std::uppercase << std::setfill('0');
+    
+    for (auto const b : bytes) {
+        std::cout << std::setw(2) << std::to_integer<int>(b) << ' ';
+    }
+    
+    std::cout << std::dec << "}\n";
+}
+
+TEST_CASE("span of bytes")
+{
+    float data[1] { 3.141592f };
+
+    auto const const_bytes = std::as_bytes(std::span{ data });
+
+    print(data[0], const_bytes);
+
+    auto const writable_bytes = std::as_writable_bytes(std::span{ data });
+ 
+    // Change the sign bit that is the MSB (IEEE 754 Floating-Point Standard).
+    writable_bytes[3] |= std::byte{ 0b1000'0000 };
+ 
+    print(data[0], const_bytes);
+}
+
+TEST_CASE("bitset")
+{
+    std::bitset<8> bs1{"01010101"};
+
+    std::cout << bs1.to_string() << " - " << bs1.to_ulong() << "\n";
 }
