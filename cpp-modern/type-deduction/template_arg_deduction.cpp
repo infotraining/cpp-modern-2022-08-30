2b6d89e67e0fec9f21080ee4b7919392aa52b4b9
Krystian Piękoś <krystian.piekos@infotraining.pl>
2022-08-30 15:55:58 +0200

    auto

--- cpp-modern/type-deduction/template_arg_deduction.cpp
+++ cpp-modern/type-deduction/template_arg_deduction.cpp
@@ -20,8 +20,8 @@ void foo(int)
 {
 }
 
-template <typename T>
-void deduce1(T arg)
+//template <typename T>
+void deduce1(auto arg)
 {
     puts(PRINT_SIGNATURE);
 }
@@ -47,6 +47,24 @@ TEST_CASE("Type deduction rules")
         int& ref_x = x;
         const int& cref_x = x;
         int tab[10];
+
+        auto a1 = x;      // int
+        deduce1(x);
+
+        auto a2 = cx;     // int
+        deduce1(cx);
+
+        auto a3 = ref_x;  // int
+        deduce1(ref_x);
+
+        auto a4 = cref_x; // int
+        deduce1(cref_x); 
+
+        auto a5 = tab;    // int* : array decays to pointer
+        deduce1(tab);
+
+        auto a6 = foo; // void (*a6)(int) : function decays to pointer
+        deduce1(foo);
     }
 
     cout << "\n";
@@ -59,7 +77,23 @@ TEST_CASE("Type deduction rules")
         const int& cref_x = x;
         int tab[10];
 
-        
+        auto& a1 = x;  // int&
+        deduce2(x);
+
+        auto& a2 = cx; // const int&
+        deduce2(cx);
+
+        auto& a3 = ref_x; // int&
+        deduce2(ref_x);
+
+        auto& a4 = cref_x; // const int&
+        deduce2(cref_x);
+
+        auto& a5 = tab; // int (&)[10] : does not decay
+        deduce2(tab);
+
+        auto& a6 = foo; // int (&)() :  does not decay
+        deduce2(foo);        
     }
 
     cout << "\n";
@@ -71,5 +105,48 @@ TEST_CASE("Type deduction rules")
         int& ref_x = x;
         const int& cref_x = x;
         int tab[10];
+
+        auto&& a1 = x;  // x is lvalue -> int&
+        deduce3(x);     // deduce3<int&>(int& && arg -> int& arg)
+
+        auto&& a2 = 42; // 42 is rvalue -> int&&
+        deduce3(42);
     }
+}
+
+template <typename T>
+class Container
+{
+    std::vector<T> items_;
+public:
+    Container() = default;
+    
+    void push_back(const T& item)
+    {
+        items_.push_back(item);
+    }
+
+    void push_back(T&& item) // always rvalue ref - no universal reference
+    {
+        items_.push_back(std::move(item));
+    }
+
+    template <typename... Ts>
+    void emplace_back(Ts&&... args)
+    {
+        items_.emplace_back(std::forward<Ts>(args)...);
+    }
+};
+
+TEST_CASE("using container")
+{
+    using namespace std::literals;
+
+    Container<std::string> words;
+
+    auto w = "text"s;
+
+    words.push_back(w);
+    words.push_back("hello"s);
+    words.emplace_back(10, 'a');
 }
