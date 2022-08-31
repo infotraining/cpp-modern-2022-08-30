31e388aa9c47efcc238ddccf62c4e3bccccc8dfa
Krystian Piękoś <krystian.piekos@infotraining.pl>
2022-08-31 12:05:22 +0200

    perfect forwarding

--- cpp-modern/type-deduction/auto_declarations_ex.cpp
+++ cpp-modern/type-deduction/auto_declarations_ex.cpp
@@ -18,74 +18,74 @@ TEST_CASE("exercise")
     const vector<int> cvec = {1, 2, 3};
 
     auto a1 = x;
-    static_assert(is_same<TODO, decltype(a1)>::value, "Error");
+    static_assert(is_same<int, decltype(a1)>::value, "Error");
 
     auto a2 = &x;
-    static_assert(is_same<TODO, decltype(a2)>::value, "Error");
+    static_assert(is_same<int*, decltype(a2)>::value, "Error");
 
     const auto a3 = &x;
-    static_assert(is_same<TODO, decltype(a3)>::value, "Error");
+    static_assert(is_same<int* const, decltype(a3)>::value, "Error");
 
     auto* a4 = &x;
-    static_assert(is_same<TODO, decltype(a4)>::value, "Error");
+    static_assert(is_same<int*, decltype(a4)>::value, "Error");
 
     auto a5 = &cx;
-    static_assert(is_same<TODO, decltype(a5)>::value, "Error");
+    static_assert(is_same<const int*, decltype(a5)>::value, "Error");
 
     auto* a6 = &cx;
-    static_assert(is_same<TODO, decltype(a6)>::value, "Error");
+    static_assert(is_same<const int*, decltype(a6)>::value, "Error");
 
     int& rx = x;
     const int& crx = x;
 
     const auto& a7 = rx;
-    static_assert(is_same<TODO, decltype(a7)>::value, "Error");
+    static_assert(is_same<const int&, decltype(a7)>::value, "Error");
 
     auto a8 = crx;
-    static_assert(is_same<TODO, decltype(a8)>::value, "Error");
+    static_assert(is_same<int, decltype(a8)>::value, "Error");
 
     auto& a9 = crx;
-    static_assert(is_same<TODO, decltype(a9)>::value, "Error");
+    static_assert(is_same<const int&, decltype(a9)>::value, "Error");
 
     auto a10 = tab;
-    static_assert(is_same<TODO, decltype(a10)>::value, "Error");
+    static_assert(is_same<int*, decltype(a10)>::value, "Error");
 
     auto& a11 = tab;
-    static_assert(is_same<TODO, decltype(a11)>::value, "Error");
+    static_assert(is_same<int(&)[10], decltype(a11)>::value, "Error");
 
     auto a12 = vec.begin();
-    static_assert(is_same<TODO, decltype(a12)>::value, "Error");
+    static_assert(is_same<vector<int>::iterator, decltype(a12)>::value, "Error");
 
     auto a13 = vec.cbegin();
-    static_assert(is_same<TODO, decltype(a13)>::value, "Error");
+    static_assert(is_same<vector<int>::const_iterator, decltype(a13)>::value, "Error");
 
     auto a14 = vec.back();
-    static_assert(is_same<TODO, decltype(a14)>::value, "Error");
+    static_assert(is_same<int, decltype(a14)>::value, "Error");
 
     auto& a15 = cvec.back();
-    static_assert(is_same<TODO, decltype(a15)>::value, "Error");
+    static_assert(is_same<const int&, decltype(a15)>::value, "Error");
 
     const auto a16 = vec[0];
-    static_assert(is_same<TODO, decltype(a16)>::value, "Error");
+    static_assert(is_same<const int, decltype(a16)>::value, "Error");
 
     auto&& a17 = 7.5;
-    static_assert(is_same<TODO, decltype(a17)>::value, "Error");
+    static_assert(is_same<double&&, decltype(a17)>::value, "Error");
 
     auto&& a18 = crx;
-    static_assert(is_same<TODO, decltype(a18)>::value, "Error");
+    static_assert(is_same<const int&, decltype(a18)>::value, "Error");
 
     auto&& a19 = vec.front();
-    static_assert(is_same<TODO, decltype(a19)>::value, "Error");
+    static_assert(is_same<int&, decltype(a19)>::value, "Error");
 
     auto&& a20 = vec.begin();
-    static_assert(is_same<TODO, decltype(a20)>::value, "Error");
+    static_assert(is_same<vector<int>::iterator&&, decltype(a20)>::value, "Error");
 
     auto a21 = {1, 2, 3};
-    static_assert(is_same<TODO, decltype(a21)>::value, "Error");
+    static_assert(is_same<initializer_list<int>, decltype(a21)>::value, "Error");
 
     auto a22{1};
-    static_assert(is_same<TODO, decltype(a22)>::value, "Error");
+    static_assert(is_same<int, decltype(a22)>::value, "Error");
 
     auto a23 = {"text"s};
-    static_assert(is_same<TODO, decltype(a23)>::value, "Error");
+    static_assert(is_same<initializer_list<string>, decltype(a23)>::value, "Error");
 }
--- cpp-modern/type-deduction/template_arg_deduction.cpp
+++ cpp-modern/type-deduction/template_arg_deduction.cpp
@@ -2,9 +2,11 @@
 
 #include <algorithm>
 #include <iostream>
+#include <map>
 #include <numeric>
 #include <string>
 #include <vector>
+#include <optional>
 
 #define MSVC
 
@@ -20,7 +22,7 @@ void foo(int)
 {
 }
 
-//template <typename T>
+// template <typename T>
 void deduce1(auto arg)
 {
     puts(PRINT_SIGNATURE);
@@ -48,19 +50,19 @@ TEST_CASE("Type deduction rules")
         const int& cref_x = x;
         int tab[10];
 
-        auto a1 = x;      // int
+        auto a1 = x; // int
         deduce1(x);
 
-        auto a2 = cx;     // int
+        auto a2 = cx; // int
         deduce1(cx);
 
-        auto a3 = ref_x;  // int
+        auto a3 = ref_x; // int
         deduce1(ref_x);
 
         auto a4 = cref_x; // int
-        deduce1(cref_x); 
+        deduce1(cref_x);
 
-        auto a5 = tab;    // int* : array decays to pointer
+        auto a5 = tab; // int* : array decays to pointer
         deduce1(tab);
 
         auto a6 = foo; // void (*a6)(int) : function decays to pointer
@@ -77,7 +79,7 @@ TEST_CASE("Type deduction rules")
         const int& cref_x = x;
         int tab[10];
 
-        auto& a1 = x;  // int&
+        auto& a1 = x; // int&
         deduce2(x);
 
         auto& a2 = cx; // const int&
@@ -93,7 +95,7 @@ TEST_CASE("Type deduction rules")
         deduce2(tab);
 
         auto& a6 = foo; // int (&)() :  does not decay
-        deduce2(foo);        
+        deduce2(foo);
     }
 
     cout << "\n";
@@ -106,8 +108,8 @@ TEST_CASE("Type deduction rules")
         const int& cref_x = x;
         int tab[10];
 
-        auto&& a1 = x;  // x is lvalue -> int&
-        deduce3(x);     // deduce3<int&>(int& && arg -> int& arg)
+        auto&& a1 = x; // x is lvalue -> int&
+        deduce3(x);    // deduce3<int&>(int& && arg -> int& arg)
 
         auto&& a2 = 42; // 42 is rvalue -> int&&
         deduce3(42);
@@ -118,17 +120,18 @@ template <typename T>
 class Container
 {
     std::vector<T> items_;
+
 public:
     Container() = default;
-    
-    void push_back(const T& item)
+
+    void push_back(const T& item) // push_back - makes copy
     {
         items_.push_back(item);
     }
 
     void push_back(T&& item) // always rvalue ref - no universal reference
     {
-        items_.push_back(std::move(item));
+        items_.push_back(std::move(item));  // push_back - make move
     }
 
     template <typename... Ts>
@@ -153,31 +156,163 @@ TEST_CASE("using container")
 
 TEST_CASE("auto + {}")
 {
-    auto lst = {1, 2, 3};  // std::initializer_list<int>
+    auto lst = {1, 2, 3}; // std::initializer_list<int>
 
     SECTION("Before C++17")
     {
         int x1 = 10;
         int x2(10);
-        int x3{10};
+        int x3 {10};
 
         auto a1 = 10; // int
         auto a2(10);  // int
-        auto a3{10};  // std::initializer_list<int>
+        auto a3 {10}; // std::initializer_list<int>
     }
 
     SECTION("Before C++17")
     {
         int x1 = 10;
         int x2(10);
-        int x3{10};
+        int x3 {10};
 
         auto a1 = 10; // int
         auto a2(10);  // int
-        auto a3{10};  // int
-        //auto a4{10, 20}; // syntax error
+        auto a3 {10}; // int
+        // auto a4{10, 20}; // syntax error
 
-        auto a4 = {1}; // std::initializer_list<int>
+        auto a4 = {1};       // std::initializer_list<int>
         auto a5 = {1, 2, 3}; // std::initializer_list<int>
     }
+}
+
+////////////////////////////////////////////
+// decltype
+
+namespace Cpp11
+{
+    template <typename T1, typename T2>
+    auto multiply(T1 a, T2 b) -> decltype(a * b)
+    {
+        return a * b;
+    }
+}
+
+template <typename T1, typename T2>
+auto multiply(T1 a, T2 b)
+{
+    return a * b;
+}
+
+template <typename T>
+auto bar()
+{
+    std::vector<decltype(std::declval<T>().foo())> foo_values;
+
+    //...
+
+    return foo_values;
+}
+
+TEST_CASE("decltype")
+{
+    std::map<int, std::string> dict = {{1, "one"}, {2, "two"}};
+
+    auto dict_alt_1 = dict;
+    REQUIRE(dict_alt_1.size() == 2);
+
+    decltype(dict) dict_alt_2;
+    REQUIRE(dict_alt_2.size() == 0);
+
+    decltype(dict)::value_type value;
+
+    auto result = Cpp11::multiply(1, 3.14);
+}
+
+
+template <typename TContainer>
+decltype(auto) get_nth(TContainer& container, size_t n)
+{
+    return container[n];
+}
+
+TEST_CASE("auto vs. decltype auto")
+{
+    std::vector vec = {1, 2, 3};
+
+    get_nth(vec, 2) = 5;
+
+    REQUIRE(get_nth(vec, 2) == 5);
+
+    SECTION("vector<bool>")
+    {
+        std::vector<bool> vec = {true, false, false};
+
+        get_nth(vec, 2) = true;
+
+        REQUIRE(get_nth(vec, 2) == true);
+    }
+}
+
+
+template <typename T>
+auto create_vec(T&& value)
+{
+    using TValue = std::decay_t<T>;
+    
+    std::vector<TValue> vec;
+
+    // if constexpr (std::is_reference_v<T>)
+    //     vec.push_back(value);  // makes copy
+    // else
+    //     vec.push_back(std::move(value));
+
+    vec.push_back(std::forward<T>(value)); // perfect forwarding
+
+    return vec;
+}
+
+namespace Explain
+{
+    template <typename T, typename... TArgs>
+    std::unique_ptr<T> make_unique(TArgs&&... args)
+    {
+        return std::unique_ptr<T>(new T(std::forward<TArgs>(args)...));
+    }
+}
+
+TEST_CASE("forwarding")
+{
+    std::vector<int> vec_int = create_vec(1);
+
+    std::string text = "abc";
+    std::vector<std::string> vec_str_1 = create_vec(text);
+    REQUIRE(vec_str_1.front() == text);
+
+    std::vector<std::string> vec_str_2 = create_vec(std::string("rvalue"));
+    REQUIRE(vec_str_2.front() == "rvalue");
+
+    auto ptrs_1 = create_vec(std::make_unique<int>(13));
+
+    auto ptr = Explain::make_unique<int>(42);
+    auto ptrs_2 = create_vec(std::move(ptr));
+
+    auto cstrings = create_vec("abc");
+}
+
+
+TEST_CASE("emplace")
+{
+    using namespace std::literals;
+
+    std::vector<std::string> words;
+
+    words.push_back("text"s); // move str to vec
+
+    auto text = "text"s;
+    words.push_back(text); // copy text to vec
+
+    //words.push_back(std::string(10, 'a'));
+    words.emplace_back(10, 'a');
+
+    std::cout << words.back() << "\n";
 }
