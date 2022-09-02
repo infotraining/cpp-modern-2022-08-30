fc060d2d3944e9ab1c060bfc7f0ef13609ac1e02
Krystian Piękoś <krystian.piekos@infotraining.pl>
2022-09-02 16:01:38 +0200

    ranges

--- cpp-modern/ranges/ranges.cpp
+++ cpp-modern/ranges/ranges.cpp
@@ -11,8 +11,8 @@
 
 #include "catch.hpp"
 
-template <typename T> 
-void print(const T&& rng, std::string_view desc)
+template <std::ranges::range T> 
+void print(T&& rng, std::string_view desc)
 {
 	std::cout << desc << ": [ ";
 	for(const auto& item : rng)
@@ -20,7 +20,81 @@ void print(const T&& rng, std::string_view desc)
 	std::cout << "]\n";
 }
 
+template <std::ranges::view TView> 
+void print(TView rng, std::string_view desc)
+{
+	std::cout << desc << ": [ ";
+	for(const auto& item : rng)
+		std::cout << item << " ";
+	std::cout << "]\n";
+}
+
+template <auto Value_>
+struct EndValue
+{
+    bool operator==(auto pos) const
+    {
+        return *pos == Value_;
+    }
+};
+
 TEST_CASE("ranges")
 {
 	std::vector vec = {1, 4, 6, 34, 567, 87, 23, 343, 665, 42, 12, 7, 5 };
+
+	SECTION("algorithms")
+	{
+		std::ranges::sort(vec);
+
+		print(vec, "vec");
+	}
+
+	SECTION("projections")
+	{
+		std::vector<std::string> words{"c", "c++", "c++20", "template"};
+
+		std::ranges::sort(words, [](const auto& a, const auto& b) { return a.size() < b.size() ;});
+		print(words, "words");
+
+		std::ranges::sort(words, std::greater{}, [](const auto& w) { return w.size(); });
+		print(words, "words");
+	}
+
+	SECTION("sentinels")
+	{
+		std::ranges::sort(vec.begin(), EndValue<665>(), std::greater{});
+		print(vec, "vec sorted with sentinel");
+
+		auto slice = std::ranges::subrange(vec.begin(), EndValue<665>());
+		
+		print(slice, "slice");
+	}
+
+	SECTION("views")
+	{
+		auto all_items = std::views::all(vec);
+		print(all_items, "all_items");
+
+
+		auto head = vec | std::views::take(5);
+		print(head, "head");
+
+		auto tail = vec | std::views::drop(5);
+		print(tail, "tail");
+
+		auto evens = vec
+			| std::views::filter([](int n) { return n % 2 == 0;})
+			| std::views::transform([](int n) { return n * n;})
+			| std::views::take(5)
+			| std::views::reverse;
+
+		print(evens, "evens");
+
+		std::vector evens_vec(evens.begin(), evens.end());
+
+		std::map<int, std::string> dict = { {1, "one"}, {2, "two"} };
+
+		print(dict | std::views::keys, "keys");
+		print(dict | std::views::values, "values");
+	}
 }
--- cpp-modern/traits-concepts/constraints_concepts.cpp
+++ cpp-modern/traits-concepts/constraints_concepts.cpp
@@ -10,7 +10,138 @@
 #include <tuple>
 #include <vector>
 
+template <typename T>
+concept IsPointer = requires(T ptr) { 
+    *ptr; 
+    ptr == nullptr;
+    { ptr < ptr } -> std::convertible_to<bool>;
+};
+
+template <typename T>
+concept IsLeanPointer = IsPointer<T> && requires(T x) {
+    requires sizeof(x) == sizeof(intptr_t);
+};
+
+TEST_CASE("lean pointer")
+{
+    static_assert(IsLeanPointer<std::unique_ptr<int>>);
+}
+
+namespace Step1
+{
+    template <typename T>
+        requires(!IsPointer<T>)
+    T max_value(T a, T b)
+    {
+        return a < b ? b : a;
+    }
+
+    template <typename T>
+        requires IsPointer<T>
+    auto max_value(T a, T b)
+    {
+        return max_value(*a, *b);
+    }
+}
+
+namespace Step2
+{
+    template <typename T>
+        requires(!IsPointer<T>)
+    T max_value(T a, T b)
+    {
+        return a < b ? b : a;
+    }
+
+    template <IsPointer T>
+    auto max_value(T a, T b)
+    {
+        return max_value(*a, *b);
+    }
+}
+
+template <typename T>
+    requires(!IsPointer<T>)
+T max_value(T a, T b)
+{
+    return a < b ? b : a;
+}
+
+auto max_value(IsPointer auto a, IsPointer auto b)
+    requires std::three_way_comparable_with<decltype(*a), decltype(*b)>
+{
+    return max_value(*a, *b);
+}
+
+namespace Explain
+{
+    template <IsPointer T1, IsPointer T2>
+    auto max_value(T1 a, T2 b)
+    {
+        return max_value(*a, *b);
+    }
+}
+
 TEST_CASE("constraints")
 {
-    
+    int x = 10;
+    int y = 20;
+
+    REQUIRE(max_value(x, y) == 20);
+
+    REQUIRE(max_value(&x, &y) == 20);
+
+    std::integral auto result = max_value(x, y);
+
+    auto sp1 = std::make_shared<int>(13);
+    auto sp2 = std::make_shared<int>(42);
+
+    REQUIRE(max_value(sp1, sp2) == 42);
+}
+
+template <typename T>
+concept PrintableRange = std::ranges::range<T> && requires {
+    std::cout << std::declval<std::ranges::range_value_t<T>>();
+};
+ 
+void print(PrintableRange auto const& rng)
+{
+    for(const auto& item : rng)
+        std::cout << item << " ";
+    std::cout << "\n";
+}
+
+TEST_CASE("print with concepts")
+{
+    std::vector vec = {1, 2, 3};
+    print(vec);
+
+    std::map<int, std::string> dict = {{1, "one"}};
+    //print(dict);
+}
+
+template <typename T>
+struct Value
+{
+    T value;
+
+    void print() const        
+    {
+        std::cout << value << "\n";
+    }
+
+    void print() const 
+        requires std::ranges::range<T>
+    {
+        ::print(value);
+    }
+};
+
+TEST_CASE("constraining member functions")
+{
+    Value v1{42};
+    v1.print();
+
+    Value v2{std::vector{1, 5, 6, 7, 8, 9}};
+    v2.print();
 }
